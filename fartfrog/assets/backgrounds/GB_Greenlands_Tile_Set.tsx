74bfce8ec7ac8107524b2da5138923b655f2e95b
davidmpickett <davidmpickett@gmail.com>
2023-09-17 22:30:18 -0500

    made stuff to jump on

---
+++ fartfrog/assets/backgrounds/GB_Greenlands_Tile_Set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="GB_Greenlands_Tile_Set" tilewidth="8" tileheight="8" tilecount="2940" columns="42">
+ <image source="../../../../../../BRICK 101 Dropbox/David Pickett/Games/Assets/Tilesets/GB Greenlands Files/GB_Greenlands_Tile_Set.png" width="336" height="560"/>
+</tileset>
---
+++ fartfrog/assets/backgrounds/GB_Underwaterlands_Tile_Set.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="GB_Underwaterlands_Tile_Set" tilewidth="8" tileheight="8" tilecount="2940" columns="42">
+ <image source="../../../../../../BRICK 101 Dropbox/David Pickett/Games/Assets/Tilesets/GB Underwater Lands Files/GB_Underwaterlands_Tile_Set.png" width="336" height="560"/>
+</tileset>
